5ed102a5afdadec1f685476bad62ed55bcab3622
BilgeTuna <92364036+BilgeTuna@users.noreply.github.com>
2023-02-23 17:07:47 +0300

    update

--- Strategy-Game/Assets/Scripts/PlayerMovement.cs
+++ Strategy-Game/Assets/Scripts/PlayerMovement.cs
@@ -14,13 +14,13 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Vector2 currentPos = rbody.position;
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        Vector2 inputVector = new Vector2(horizontalInput, verticalInput);
-        inputVector = Vector2.ClampMagnitude(inputVector, 1);
-        Vector2 movement = inputVector * movementSpeed;
-        Vector2 newPos = currentPos + movement * Time.deltaTime;
-        rbody.MovePosition(newPos);
+            Vector2 currentPos = rbody.position;
+            float horizontalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
+            Vector2 inputVector = new Vector2(horizontalInput, verticalInput);
+            inputVector = Vector2.ClampMagnitude(inputVector, 1);
+            Vector2 movement = inputVector * movementSpeed;
+            Vector2 newPos = currentPos + movement * Time.deltaTime;
+            rbody.MovePosition(newPos);
     }
 }
