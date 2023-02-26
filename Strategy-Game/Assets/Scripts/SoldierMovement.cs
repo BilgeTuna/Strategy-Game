da6cd2219c50e929e10e9eb30b10480c2aeb5b04
BilgeTuna <92364036+BilgeTuna@users.noreply.github.com>
2023-02-26 22:11:37 +0300

    update

--- Strategy-Game/Assets/Scripts/SoldierMovement.cs
+++ Strategy-Game/Assets/Scripts/SoldierMovement.cs
@@ -1,26 +1,81 @@
+using Cinemachine;
+using Pathfinding;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class SoldierMovement : MonoBehaviour
 {
-    public float movementSpeed = 1f;
-    Rigidbody2D rbody;
+    public CinemachineVirtualCamera virtualCamera;
+    public Animator anim;
+    public float rotationSpeed = 5f;
+    public bool turnRight = true;
 
-    private void Awake()
+    private Seeker seeker;
+    private Path path;
+    private int currentWaypoint = 0;
+    private bool isMoving = false;
+    private bool movingRight = true;
+
+    private void Start()
     {
-        rbody= GetComponent<Rigidbody2D>();
+        seeker = GetComponent<Seeker>();
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
-            Vector2 currentPos = rbody.position;
-            float horizontalInput = Input.GetAxis("Horizontal");
-            float verticalInput = Input.GetAxis("Vertical");
-            Vector2 inputVector = new Vector2(horizontalInput, verticalInput);
-            inputVector = Vector2.ClampMagnitude(inputVector, 1);
-            Vector2 movement = inputVector * movementSpeed;
-            Vector2 newPos = currentPos + movement * Time.deltaTime;
-            rbody.MovePosition(newPos);
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            seeker.StartPath(transform.position, mousePosition, OnPathComplete);
+            isMoving = true;
+        }
+
+        if (isMoving)
+        {
+            anim.SetBool("isWalinkg", true);
+
+            if (path == null)
+            {
+                return;
+            }
+
+            if (currentWaypoint >= path.vectorPath.Count)
+            {
+                isMoving = false;
+                return;
+            }
+
+            Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - (Vector2)transform.position).normalized;
+            transform.position += (Vector3)direction * Time.deltaTime * 5f;
+
+            // Check for left/right movement and log a message
+            if (direction.x > 0 && !movingRight)
+            {
+                transform.GetComponent<SpriteRenderer>().flipX = false;
+                movingRight = true;
+            }
+            else if (direction.x < 0 && movingRight)
+            {
+                transform.GetComponent<SpriteRenderer>().flipX = true;
+                movingRight = false;
+            }
+
+            if (Vector2.Distance(transform.position, path.vectorPath[currentWaypoint]) < 0.1f)
+            {
+                currentWaypoint++;
+            }
+        }
+        else if (!isMoving) anim.SetBool("isWalinkg", false);
+    }
+
+    private void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
     }
 }
