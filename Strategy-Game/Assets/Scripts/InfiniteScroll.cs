52373676de62e531d1fb8173801cf868783d966a
BilgeTuna <92364036+BilgeTuna@users.noreply.github.com>
2023-02-28 18:22:13 +0300

    update

---
+++ Strategy-Game/Assets/Scripts/InfiniteScroll.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class InfiniteScroll : MonoBehaviour, IBeginDragHandler, IDragHandler, IScrollHandler
+{
+
+    void Start()
+    {
+
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnScroll(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
