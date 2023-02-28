b24122eade2f1c45e9a6d719a76741c280bae3a2
BilgeTuna <92364036+BilgeTuna@users.noreply.github.com>
2023-02-28 23:48:44 +0300

    update

--- Strategy-Game/Assets/Scripts/InfiniteScroll.cs
+++ Strategy-Game/Assets/Scripts/InfiniteScroll.cs
@@ -2,23 +2,73 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.UI;
 
-public class InfiniteScroll : MonoBehaviour
+public class InfiniteScroll : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    [SerializeField] private Transform scrollViewContent;
-    [SerializeField] GameObject prefab;
-    [SerializeField] List<Sprite> builds;
+    public List<GameObject> itemPrefabs;
+    public float itemHeight;
+    public int maxItems;
 
-    private void Start()
+    private RectTransform contentRectTransform;
+    private List<GameObject> items = new List<GameObject>();
+    private int prefabIndex = 0;
+    private bool isPointerOver = false;
+
+    void Start()
     {
-        foreach (Sprite build in builds)
+        contentRectTransform = GetComponent<RectTransform>();
+        AddItems();
+    }
+
+    void Update()
+    {
+        if (!isPointerOver && items.Count < maxItems)
         {
-            GameObject newBuild = Instantiate(prefab, scrollViewContent);
-            if (newBuild.TryGetComponent<ScrollViewItem>(out ScrollViewItem item))
+            AddItems();
+        }
+
+        // Check if the last item has gone off-screen
+        GameObject lastItem = items[items.Count - 1];
+        if (contentRectTransform.InverseTransformPoint(lastItem.transform.position).y < contentRectTransform.rect.yMin)
+        {
+            // Move the last item to the top of the list
+            items.Remove(lastItem);
+            items.Insert(0, lastItem);
+
+            // Update the positions of all items
+            for (int i = 0; i < items.Count; i++)
             {
-                item.ChangeImage(build);
+                items[i].transform.localPosition = new Vector3(0, -itemHeight * i, 0);
             }
         }
+    }
+
+    void AddItems()
+    {
+        for (int i = items.Count; i < maxItems; i++)
+        {
+            GameObject item = Instantiate(itemPrefabs[prefabIndex]);
+            item.transform.SetParent(transform, false);
+            item.transform.localPosition = new Vector3(0, -itemHeight * items.Count, 0);
+            items.Add(item);
+
+            contentRectTransform.sizeDelta = new Vector2(contentRectTransform.sizeDelta.x, itemHeight * items.Count);
+
+            prefabIndex++;
+            if (prefabIndex >= itemPrefabs.Count)
+            {
+                prefabIndex = 0;
+            }
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        isPointerOver = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        isPointerOver = false;
     }
 }
