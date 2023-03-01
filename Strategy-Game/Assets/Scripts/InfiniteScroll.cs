3c530dfdea1ff78bece59a9576b334b2d624329d
BilgeTuna <92364036+BilgeTuna@users.noreply.github.com>
2023-03-01 23:01:46 +0300

    update

--- Strategy-Game/Assets/Scripts/InfiniteScroll.cs
+++ Strategy-Game/Assets/Scripts/InfiniteScroll.cs
@@ -3,35 +3,79 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class InfiniteScroll : MonoBehaviour
 {
-    public GameObject itemPrefab;
-    public int itemCount = 12;
-    public float itemHeight = 420f;
+    public ScrollRect scrollView;
+    public RectTransform content;
+    public float childHeight;
 
-    private RectTransform contentRect;
-    private Vector2 contentVector;
+    private int numVisibleChildren;
 
-    [SerializeField] private ObjectPool objectPool;
-
-    void Start()
+    private void Start()
     {
-        contentRect = GetComponent<RectTransform>();
-        contentRect.sizeDelta = new Vector2(contentRect.sizeDelta.x, itemCount * itemHeight);
+        // Calculate the total height of the content
+        float contentHeight = content.sizeDelta.y;
 
-        for (int i = 0; i < itemCount; i++)
+        // Calculate the number of visible children in the scroll view
+        numVisibleChildren = Mathf.RoundToInt(scrollView.GetComponent<RectTransform>().rect.height / childHeight) + 1;
+
+        // If the number of visible children is greater than the total number of children, we don't need to loop
+        if (numVisibleChildren >= content.childCount)
         {
-            //GameObject newItem = Instantiate(itemPrefab, transform);
-            GameObject gameObject = objectPool.GetPooledObject();
-            gameObject.transform.parent = transform.parent;
-            gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, -i * itemHeight);
+            return;
+        }
+
+        // Duplicate the first visible children and append them to the end of the content
+        for (int i = 0; i < numVisibleChildren; i++)
+        {
+            RectTransform child = content.GetChild(i).GetComponent<RectTransform>();
+            RectTransform duplicate = Instantiate(child, content);
+            duplicate.anchoredPosition += new Vector2(0f, contentHeight);
         }
     }
 
-    public void OnScrollValueChanged(Vector2 scrollValue)
+    private void OnEnable()
     {
-        contentVector += scrollValue * 10f;
-        contentRect.anchoredPosition = contentVector;
+        // Subscribe to the onValueChanged event of the ScrollRect component
+        scrollView.onValueChanged.AddListener(OnScrollValueChanged);
+    }
+
+    private void OnDisable()
+    {
+        // Unsubscribe from the onValueChanged event of the ScrollRect component
+        scrollView.onValueChanged.RemoveListener(OnScrollValueChanged);
+    }
+
+    private void OnScrollValueChanged(Vector2 scrollPos)
+    {
+        // Get the last child of the content
+        RectTransform lastChild = content.GetChild(content.childCount - 1).GetComponent<RectTransform>();
+
+        // If the last child is fully visible, we need to loop
+        if (lastChild.anchoredPosition.y + lastChild.sizeDelta.y <= scrollView.GetComponent<RectTransform>().rect.height)
+        {
+            // Get the first visible child
+            RectTransform firstChild = content.GetChild(0).GetComponent<RectTransform>();
+
+            // Move it to the end of the content
+            firstChild.anchoredPosition += new Vector2(0f, content.sizeDelta.y);
+
+            // Set it as the last child
+            firstChild.SetAsLastSibling();
+        }
+        // If the first child is fully visible, we need to loop backwards
+        else if (content.GetChild(0).GetComponent<RectTransform>().anchoredPosition.y >= 0)
+        {
+            // Get the last visible child
+            RectTransform lastVisibleChild = content.GetChild(numVisibleChildren - 1).GetComponent<RectTransform>();
+
+            // Move it to the beginning of the content
+            lastVisibleChild.anchoredPosition -= new Vector2(0f, content.sizeDelta.y);
+
+            // Set it as the first child
+            lastVisibleChild.SetAsFirstSibling();
+        }
     }
 }
--- Strategy-Game/Assets/Scripts/ObjectPool.cs
+++ Strategy-Game/Assets/Scripts/ObjectPool.cs
@@ -6,21 +6,28 @@ public class ObjectPool : MonoBehaviour
 {
     private Queue<GameObject> pooledObjects;
     [SerializeField] private GameObject objectPrefab;
-    [SerializeField] private int poolSize;
+    [SerializeField] public int poolSize;
 
-    private void Awake()
+    public void Awake()
     {
-        pooledObjects= new Queue<GameObject>();
+        pooledObjects = new Queue<GameObject>();
         for (int i = 0; i < poolSize; i++)
         {
             GameObject gameObject = Instantiate(objectPrefab);
+            gameObject.SetActive(false);
             pooledObjects.Enqueue(gameObject);
         }
+    }
+
+    public GameObject GetDeleteIt()
+    { 
+        return pooledObjects.Dequeue();
     }
 
     public GameObject GetPooledObject()
     {
         GameObject gameObject = pooledObjects.Dequeue();
+        gameObject.SetActive(true);
         pooledObjects.Enqueue(gameObject);
         return gameObject;
     }
